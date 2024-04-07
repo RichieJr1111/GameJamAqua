a17642ba64ccf9fba2e11b08b985e46b2c8aad11
RichieJr1111 <emailrichiejr@gmail.com>
2024-04-06 17:09:42 -0700

    Sound Changes

--- Assets/Scripts/JukeBox.cs
+++ Assets/Scripts/JukeBox.cs
@@ -16,23 +16,52 @@ public class JukeBox : MonoBehaviour
     // Update is called once per frame
     public void WhatShouldPlay()
     {
-        if (Time.timeScale <= 0 && !Shop.isPlaying)
+        if (Time.timeScale <= 0 && Shop.volume == 0f)
         {
             //play shop
-            Battle.Stop();
-            Plan.Stop();
+            //Shop.mute = false;
+            //Battle.mute = true;
+            //Plan.mute = true;
+            StartCoroutine(SlowTransition(false, true, true));
         }
-        else if (GameObject.FindGameObjectWithTag("GoodGuy") == null && !Plan.isPlaying)
+        else if (GameObject.FindGameObjectWithTag("GoodGuy") == null && Plan.volume == 0f)
         {
             //play plan
-            Plan.Play();
-            Battle.Stop();
+            //Battle.mute = true;
+            //Plan.mute = false;
+            //Shop.mute = true;
+            StartCoroutine(SlowTransition(true, false, true));
         }
-        else if(!Battle.isPlaying)
+        else if(Battle.volume == 0f)
         {
             //play battle
-            Battle.Play();
-            Plan.Stop();
+            //Plan.mute = true;
+            //Shop.mute = true;
+            //Battle.mute = false;
+            StartCoroutine(SlowTransition(true, true, false));
+        }
+    }
+
+    private IEnumerator SlowTransition(bool one, bool two, bool three)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            if (!one)
+            {
+                Shop.volume += 0.01f;
+                Battle.volume -= 0.01f;
+            }
+            else if (!two)
+            {
+                Plan.volume += 0.01f;
+                Shop.volume -= 0.01f;
+            }
+            else if (!three)
+            {
+                Plan.volume -= 0.01f;
+                Battle.volume += 0.01f;
+            }
+            yield return new WaitForSeconds(0.01f);
         }
     }
 }
